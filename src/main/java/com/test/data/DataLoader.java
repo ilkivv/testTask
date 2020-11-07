f1769e0979999a79ccc69bd825371c478d9822d8
aleksey <ilkivv@yandex.ru>
2020-11-07 11:56:57 +0700

    добавление сущности user

---
+++ src/main/java/com/test/data/DataLoader.java
@@ -0,0 +1,18 @@
+package com.test.data;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataLoader implements ApplicationRunner {
+
+    public DataLoader(){
+
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/test/entities/User.java
@@ -0,0 +1,33 @@
+package com.test.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String full_name;
+
+    public User(){}
+
+    public User(String full_name){
+        this.full_name = full_name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+}
